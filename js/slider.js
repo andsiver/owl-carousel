456d2c17f8c6b86ca9ee5a967d0d16e0d0104a6f
Andrii Siver <andrewsiver7@gmail.com>
2021-01-04 15:23:16 +0200

    The owl plugin has been added and adjusted

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:true
+            },
+            1200:{
+                items:2,
+                nav:false
+            }
+        }
+    })
+  });
